5352421937c843a1c2c1abda1fc7a2d2c5d08fa1
henrylin2015 <lin_hailing@sina.com>
2016-04-07 17:24:50 +0800

    create menu,accessToken

--- WeChat.class.php
+++ WeChat.class.php
@@ -1,8 +1,12 @@
 <?php 
 class WeChat {
-    private $_appId;
-    private $_appSecret;
+    private $_appid;
+    private $_appsecret;
     private $_token;
+    public function __construct($appid,$appsecret){
+        $this->_appid = $appid;
+        $this->_appsecret = $appsecret;
+    }
     public function _request($curl, $https = true, $method = 'GET', $data = null) {
         $ch = curl_init();
         //set url 
@@ -11,8 +15,8 @@ class WeChat {
         //return transfer as a string
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         if ($https) {
-            curl_setopt($cH, CURLOPT_SSL_VERIFYHOST, TRUE);
-            curl_setopt($cH, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         }
         //POST
         if ($method == "POST") {
@@ -24,5 +28,39 @@ class WeChat {
         //close 
         curl_close($ch);
         return $output;
+    }
+    /***
+    * @description:获取token
+    * @auther:henry
+    * @create:2016年04月07日16:49:07
+    * @return:token
+    */
+    public function _getAccessToken(){
+        $file = "./AccessToken";
+        if(file_exists($file)){
+            $content = json_decode(file_get_contents($file));
+            if(time()- filemtime($file) < $content->expires_in){
+                return $content->access_token;
+            }
+        }
+        $curl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s";
+        $curl = sprintf($curl,$this->_appid,$this->_appsecret);
+        $content = $this->_request($curl);
+        file_put_contents($file,$content);
+        $content = json_decode($content);
+        return $content->access_token;
+    }
+    /***
+    * @description:创建菜单
+    * @auther:henry
+    * @create:2016年04月07日16:49:07
+    * @return:menu
+    */
+    public function _createMenu($data){
+        $this->_token = $this->_getAccessToken();
+        $curl = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s";
+        $curl = sprintf($curl,$this->_token);
+        $content = $this->_request($curl,true,'POST',$data);
+        return $content;
     }
 }
--- index.php
+++ index.php
@@ -12,5 +12,34 @@ if (function_exists($f)) {
 }
 echo "time".mt_rand(1, time())."<br>";
 */
-$weChat = new  WeChat();
-echo $weChat->_request("https://www.baidu.com/");
+$weChat = new  WeChat("wx5da47318d62796fa","81b21cf819fe6d0fd3491ae020ac3a34","");
+//echo $weChat->_request("https://www.baidu.com/");
+//echo $weChat->_getAccessToken();
+$data = '{
+     "button":[
+     {	
+          "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"菜单",
+           "sub_button":[
+           {	
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+               "type":"view",
+               "name":"视频",
+               "url":"http://v.qq.com/"
+            },
+            {
+               "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
+ }';
+ echo $weChat->_createMenu($data);
